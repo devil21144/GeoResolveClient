fad1d1b25fd100b46f63786b932bd6b98c363309
Dakshina <mihirvelaga21144@gmail.com>
2025-06-13 12:32:53 +0530

    Add files via upload

---
+++ src/Components/CitizenRegistration.jsx
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+import Tooltip from "@mui/material/Tooltip";
+import TextField from "@mui/material/TextField";
+import axios from "axios";
+import { CheckIcon } from "lucide-react";
+import { House, LogIn, UserRoundSearch } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+const CitizenRegistration = () => {
+  const [username, setUsername] = useState();
+  const [age, setAge] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+  const [phoneno, setPhno] = useState();
+  const [errr, setErr] = useState(false);
+  const [errmessage, setErrMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const handleSubmit = async () => {
+    try {
+      const results = await axios.post(
+        "http://localhost:5001/register/citizen",
+        {
+          username,
+          age,
+          password,
+          email,
+          phoneno,
+        }
+      );
+      setSuccess(true);
+      setErr(false);
+    } catch (err) {
+      const message =
+        err.response?.message || err.message || "Internal Server error";
+      const code = err.response?.status || err.statusCode || 500;
+      setErr(true);
+      setSuccess(false);
+      console.log(message);
+      setErrMessage(message);
+    }
+  };
+  const navigate = useNavigate();
+  return (
+    <div className="h-dvh w-dvw">
+      <nav
+        className={`h-16 bg-indigo-600 flex flex-row justify-between items-center`}
+      >
+        <div>
+          <p className="text-white font-medium text-xl ml-4">GeoResolve</p>
+        </div>
+        <div
+          id="buttonsDiv"
+          className="text-white w-64 me-4 flex justify-evenly"
+        >
+          <Tooltip title="Home" placement="bottom">
+            <button
+              onClick={() => {
+                navigate("/");
+              }}
+              className="cursor-pointer"
+            >
+              <House />
+            </button>
+          </Tooltip>
+          <Tooltip title="Login" placement="bottom">
+            <button
+              onClick={() => {
+                navigate("/login");
+              }}
+              className="cursor-pointer"
+            >
+              <LogIn />
+            </button>
+          </Tooltip>
+          <Tooltip title="Search Authority" placement="bottom">
+            <button
+              onClick={() => {
+                navigate("/searchauthority");
+              }}
+              className="cursor-pointer"
+            >
+              <UserRoundSearch />
+            </button>
+          </Tooltip>
+          <Tooltip title="Main Register Page" placement="bottom">
+            <button
+              onClick={() => {
+                navigate("/register");
+              }}
+              className="font-bold"
+            >
+              R
+            </button>
+          </Tooltip>
+        </div>
+      </nav>
+      <div
+        id="remainingDiv"
+        className="h-dvh flex items-center justify-center"
+      >
+        <div
+          id="subDiv"
+          className="h-full w-full border border-indigo-600 flex flex-col justify-evenly items-center"
+        >
+          <h1 className="text-3xl">Citizens' Registration</h1>
+          <TextField
+            id="demo-helper-text-misaligned-no-helper"
+            label="Username"
+            value={username}
+            required
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <TextField
+            id="demo-helper-text-misaligned-no-helper"
+            label="Password"
+            inputProps={{ type: "password" }}
+            value={password}
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <TextField
+            inputProps={{ type: "number" }}
+            label="Age"
+            required
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+          <TextField
+            id="demo-helper-text-misaligned-no-helper"
+            label="Email"
+            value={email}
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <TextField
+            id="demo-helper-text-misaligned-no-helper"
+            label="Phone Number"
+            value={phoneno}
+            required
+            onChange={(e) => {
+              setPhno(e.target.value);
+            }}
+          />
+          {errr && <Alert severity="error">{errmessage}</Alert>}
+          {success && (
+            <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+              Registration Successful
+            </Alert>
+          )}
+          <Button onClick={handleSubmit} variant="contained" color="secondary">
+            Submit
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CitizenRegistration;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {
+  Menu,
+  LogIn,
+  X,
+  House,
+  UserRound,
+  UserSearch,
+} from "lucide-react";
+const Home = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <div className="h-dvh w-dvw">
+      <div
+        id="sidebarNav"
+        className={`bg-indigo-600 transition-all duration-500 ease-out-in ${
+          sidebarOpen ? "w-40" : "w-16"
+        } h-screen flex-col text-center`}
+      >
+        <div>
+          <button
+            onClick={() => {
+              setSidebarOpen(!sidebarOpen);
+            }}
+            className="self-start mt-2 text-white"
+          >
+            {sidebarOpen ? <X /> : <Menu />}
+          </button>
+        </div>
+        <span className="text-white underline decoration-solid">
+          {sidebarOpen ? "Menu" : ""}
+        </span>
+        <div className="flex-col items-center">
+          <nav className="text-white mt-6 flex flex-col items-center">
+            <a href="#">{sidebarOpen ? "Home" : <House />}</a>
+            <br />
+            <a href="#">{sidebarOpen ? "Login" : <LogIn />}</a>
+            <br />
+            <a href="/register">{sidebarOpen ? "Register" : "R"}</a>
+            <br />
+            <a href="#">{sidebarOpen ? "About Us" : <UserRound />}</a>
+            <br />
+            <a href="#">{sidebarOpen ? "Search Authority" : <UserSearch />}</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
