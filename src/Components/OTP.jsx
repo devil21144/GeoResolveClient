13216e49cd7a5530b79a188750b8769fbae68ab4
Dakshina <mihirvelaga21144@gmail.com>
2025-06-13 19:25:19 +0530

    Add files via upload

---
+++ src/Components/OTP.jsx
@@ -0,0 +1,44 @@
+import TextField from "@mui/material/TextField";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import axios from "axios";
+const OTP = () => {
+  const [otp, setOtp] = useState();
+  const [err, setError] = useState();
+  const [success, setSuccess] = useState();
+  const [errMessage, setErrorMessage] = useState();
+  const handleSubmit = async () => {
+    const username = window.sessionStorage.getItem('username');
+    try {
+      const results = await axios.post('http://localhost:5001/otp', {
+        otp,
+        username
+      })
+      setError('false');
+      setSuccess(true);
+    }
+    catch (err) {
+      const message = err.response?.data?.message || err.message || 'Internal Server Error';
+      setError('true');
+      setErrorMessage(message);
+      setSuccess(false);
+    }
+  }
+  return (
+    <div className="h-dvh w-dvw flex items-center justify-center">
+      <div id="subDiv" className="h-1/3 w-1/3 border border-indigo-600 flex  flex-col items-center justify-evenly">
+        <h1>OTP Verification</h1>
+        <TextField
+          inputProps={{ type: "number" }}
+          value={otp}
+          label="OTP"
+          required
+        />
+        <Button variant="contained" color="secondary">Verify</Button>
+      </div>
+    </div>
+  );
+};
+
+export default OTP;
