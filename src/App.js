20b6521c275d50e3b51dcb607c4b4ffa890e71cc
Dakshina <mihirvelaga21144@gmail.com>
2025-06-18 10:49:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ import Register from "./Components/Register";
 import AuthorityRegister from "./Components/AuthorityRegister";
 import CitizenRegistration from "./Components/CitizenRegistration";
 import OTP from "./Components/OTP";
+import AdminDashboard from "./Components/AdminDashboard";
 import CitizenLogin from "./Components/CitizenLogin";
 import AuthorityLogin from "./Components/AuthorityLogin";
 import AdminLogin from "./Components/AdminLogin";
+import CitizenDashboard from "./Components/CitizenDashboard";
+import AuthorityDashboard from "./Components/AuthorityDashboard";
+import AdminProtectedRoute from "./Components/AdminProtectedRoute";
+import AuthorityProtectedRoute from "./Components/AuthorityProtectedRoute";
+import CitizenProtectedRoute from "./Components/CitizenProtectedRoute";
 
 const App = () => {
   return (
@@ -18,12 +24,32 @@ const App = () => {
         <Route element={<Home />} path="/"></Route>
         <Route element={<Login />} path="/login"></Route>
         <Route element={<Register />} path="/register"></Route>
-        <Route element={<AuthorityRegister />} path="/register/authority"></Route>
-        <Route element={<CitizenRegistration />} path="/register/citizen"> </Route>
+        <Route
+          element={<AuthorityRegister />}
+          path="/register/authority"
+        ></Route>
+        <Route element={<CitizenRegistration />} path="/register/citizen">
+          {" "}
+        </Route>
         <Route element={<OTP />} path="/otp"></Route>
         <Route path="/login/citizen" element={<CitizenLogin />}></Route>
         <Route path="/login/authority" element={<AuthorityLogin />}></Route>
-        <Route path="/login/admin" element={<AdminLogin/>}></Route>
+        <Route path="/login/admin" element={<AdminLogin />}></Route>
+        <Route element={<AdminProtectedRoute />}>
+          <Route path="/dashboard/admin" element={<AdminDashboard />}></Route>
+        </Route>
+        <Route element={<CitizenProtectedRoute />}>
+          <Route
+            path="/dashboard/citizen"
+            element={<CitizenDashboard />}
+          ></Route>
+        </Route>
+        <Route element={<AuthorityProtectedRoute />}>
+          <Route
+            path="/dashboard/authority"
+            element={<AuthorityDashboard />}
+          ></Route>
+        </Route>
       </Routes>
     </div>
   );
